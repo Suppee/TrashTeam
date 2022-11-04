af350336c0d631c34c251c5eb1a09a980d602f46
magnus <mkofo20@student.sdu.dk>
2022-11-04 09:19:28 +0100

    Auto stash before merge of "main" and "origin/main"

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let processScroll = () => {
         scrollPercent = scrollTop / scrollBottom * 100 + '%';
 
     document.getElementById('progressbar').style.setProperty('--scrollAmount', scrollPercent);
+    console.log(scrollPercent);
 }
 document.addEventListener('scroll',processScroll);
 
