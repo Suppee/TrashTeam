63b16e33ca6865138d110b0bd2cc20a25738e27c
magnus kofoed <magnus@kofoed2010.dk>
2022-11-30 17:19:45 +0100

    reverrse scroll

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 
-$(window).on("scroll", function () {
-    $(".right").css("bottom", $(window).scrollTop() * -1);
-});
-
 let processScroll = () => {
     let docElem = document.documentElement,
         docBody = document.body,
         scrollTop = docElem['scrollTop'] || docBody['scrollTop']
-    scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+        scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
         scrollPercent = scrollTop / scrollBottom * 100 + '%';
 
     document.getElementById('progressbar').style.setProperty('--scrollAmount', scrollPercent);
     console.log(scrollPercent);
 }
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){
+      $('.ScollBody').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)'); 
+   }).scroll();
+});  
 document.addEventListener('scroll', processScroll);
-
-
-
-
