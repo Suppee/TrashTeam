4d6082940baf5b92c21689b3887423398c56dd19
Christian <CStochholm@kami-io.com>
2022-12-01 14:43:26 +0100

    clean up

--- main.js
+++ main.js
@@ -9,10 +9,3 @@ let processScroll = () => {
     document.getElementById('progressbar').style.setProperty('--scrollAmount', scrollPercent);
     console.log(scrollPercent);
 }
-
-$(document).ready(function(){ 
-    $(window).scroll(function(){
-      $('.ScollBody').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)'); 
-   }).scroll();
-});  
-document.addEventListener('scroll', processScroll);
