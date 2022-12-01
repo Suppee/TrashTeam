89c6e0cb24e25ec707137b42231f063bbc9c5d51
magnus kofoed <magnus@kofoed2010.dk>
2022-12-01 15:00:36 +0100

    reverse load

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ let processScroll = () => {
         docBody = document.body,
         scrollTop = docElem['scrollTop'] || docBody['scrollTop']
         scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
-        scrollPercent = scrollTop / scrollBottom * 100 + '%';
+        scrollPercent =  (scrollTop/scrollBottom  * 100) + '%';
 
     document.getElementById('progressbar').style.setProperty('--scrollAmount', scrollPercent);
     console.log(scrollPercent);
+    console.log(scrollTop);
+    console.log(scrollBottom)
 }
-
-$(document).ready(function(){ 
-    $(window).scroll(function(){
-      $('.ScollBody').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)'); 
-   }).scroll();
-});  
+window.scroll(0, document.documentElement.scrollHeight)
 document.addEventListener('scroll', processScroll);
